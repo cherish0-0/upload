41ff1808dcb7123bd1ad6b7deeb8cacaa1e5b2e9
cherish0-0 <jiyu01160@naver.com>
2025-06-18 23:40:48 +0900

    feat(fileName): 생성자 추가

--- src/main/java/springmvc/upload/domain/UploadFile.java
+++ src/main/java/springmvc/upload/domain/UploadFile.java
@@ -6,4 +6,9 @@ import lombok.Data;
 public class UploadFile {
     private String uploadFileName; // 업로드된 파일 이름
     private String storeFileName; // 저장된 파일 이름
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
 }
