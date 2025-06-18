a727762cc46f50907ef1e6858b6ee7145927f030
cherish0-0 <jiyu01160@naver.com>
2025-06-19 00:03:37 +0900

    feat(object): 필드명 변경

--- src/main/java/springmvc/upload/domain/Item.java
+++ src/main/java/springmvc/upload/domain/Item.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Item {
 
     private Long id;
-    private String name;
+    private String itemName;
     private UploadFile attachFile;
     private List<UploadFile> files;
 }
